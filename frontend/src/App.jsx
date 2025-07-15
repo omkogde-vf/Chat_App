530d97671311e55489b765ec8f212c3065d34d4a
OmK121 <omkogde121@gmail.com>
2025-07-15 18:17:28 +0530

    Added toaster

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import ProfilePage from './pages/ProfilePage'
 import NavBar from './components/Navbar'
 import { useAuthStore } from './store/useAuthStore'
 import { Loader } from 'lucide-react'
+import { Toaster } from 'react-hot-toast'
 
 const App = () => {
   const { authUser,checkAuth,isCheckingAuth }= useAuthStore();
@@ -32,10 +33,12 @@ const App = () => {
       <Routes>
         <Route path='/' element={authUser ? <HomePage />: <Navigate to='/login'/>}/>
         <Route path='/signup' element={!authUser ? <SignUpPage /> : <Navigate to='/' />}/>
-        <Route path='/login' element={<LoginPage />}/>
+        <Route path='/login' element={!authUser ? <LoginPage /> : <Navigate to='/'/>}/>
         <Route path='/settings' element={<SettingsPage />}/>
         <Route path='/profile' element={authUser ? <ProfilePage /> : <Navigate to='/login'/>}/>
       </Routes>
+
+      <Toaster />
     </div>
   )
 }
